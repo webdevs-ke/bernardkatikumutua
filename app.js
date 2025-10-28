f38c11b385d753ab1d136ce9abd7e98f099216c5
WebDevs-KE <bernardkatiku@jomcon.co.ke>
2025-10-28 03:19:27 +0300

    dark & light mode sun and moon icons switching, themed progress bar

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 (function(){
-  angular.module('portfolioApp', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider){
+  var ng = angular.module('portfolioApp', ['ngRoute']);
+    ng.directive('autoHideHeader', function() {
+      return {
+        restrict: 'A',
+        link: function(scope, element) {
+          let lastScroll = 0;
+          window.addEventListener('scroll', () => {
+            let current = window.scrollY;
+            if (current > lastScroll && current > 100) {
+              element.addClass('hide');
+            } else {
+              element.removeClass('hide');
+            }
+            lastScroll = current <= 0 ? 0 : current;
+          });
+        }
+      };
+    });
+    
+    ng.config(['$routeProvider', function($routeProvider){
       $routeProvider
         .when('/', { templateUrl: 'home.html' })
         .when('/projects', { templateUrl: 'projects.html' })
